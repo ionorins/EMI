40f7ff6253e7b59354143f6ef01879f73932c54b
Bisoros <pi@raspberrypi>
2018-05-28 11:16:56 +0000

    gpio

--- gpio.py
+++ gpio.py
@@ -1,10 +1,7 @@
-import RPi.GPIO as GPIO
-import pygame
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-GPIO.setmode(GPIO.BCM)
-
-
-GPIO.setup(7, GPIO.IN)
-
-while True:
-    print GPIO.input(7)
+while True: # Run forever
+    print GPIO.input(10)
